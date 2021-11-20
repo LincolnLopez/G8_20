df398c8c93365a4d3e88d140e29290a8764caf2a
rasalgado <rasalgadoc@unah.hn>
2021-11-19 20:46:11 -0600

    Cambio rutas

--- Pedidos/controller/pedidos.php
+++ Pedidos/controller/pedidos.php
@@ -1,12 +1,12 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
-    header('Access-Control-Allow-Headers: token, Content-Type');
-    header('Access-Control-Max-Age: 1728000');
-    header('Content-Length: 0');
-    header('Content-Type: text/plain');
-    die();
+        if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+            header('Access-Control-Allow-Origin: *');
+            header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+            header('Access-Control-Allow-Headers: token, Content-Type');
+            header('Access-Control-Max-Age: 1728000');
+            header('Content-Length: 0');
+            header('Content-Type: text/plain');
+            die();
   }
         header('Access-Control-Allow-Origin: *');
         header('Content-Type: application/json');
@@ -19,29 +19,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 
         switch($_GET["op"]){
 
-        case "GetPedidos":
-            $datos=$pedidos->get_pedidos();
-            echo json_encode($datos);
-        break;
+            case "GetPedidos":
+                $datos=$pedidos->get_pedidos();
+                echo json_encode($datos);
+            break;
 
-        case "GetPedido":
-            $datos=$pedidos->get_pedido($body["ID"]);
-            echo json_encode($datos);
-        break;
+            case "GetPedido":
+                $datos=$pedidos->get_pedido($body["ID"]);
+                echo json_encode($datos);
+            break;
 
-        case "InsertPedido":
-            $datos=$pedidos->insert_pedido($body["ID_SOCIO"],$body["FECHA_PEDIDO"],$body["DETALLE"],$body["SUB_TOTAL"],$body["TOTAL_ISV"],$body["TOTAL"],$body["FECHA_ENTREGA"]);
-            echo json_encode("Pedido Agregado");
-        break;
+            case "InsertPedido":
+                $datos=$pedidos->insert_pedido($body["ID_SOCIO"],$body["FECHA_PEDIDO"],$body["DETALLE"],$body["SUB_TOTAL"],$body["TOTAL_ISV"],$body["TOTAL"],$body["FECHA_ENTREGA"]);
+                echo json_encode("Pedido Agregado");
+            break;
 
-        case "UpdatePedido":
-            $datos=$pedidos->update_pedido($body["ID"],$body["ID_SOCIO"],$body["FECHA_PEDIDO"],$body["DETALLE"],$body["SUB_TOTAL"],$body["TOTAL_ISV"],$body["TOTAL"],$body["FECHA_ENTREGA"],$body["ESTADO"]);
-            echo json_encode("Pedido Actualizado");
-        break;
+            case "UpdatePedido":
+                $datos=$pedidos->update_pedido($body["ID"],$body["ID_SOCIO"],$body["FECHA_PEDIDO"],$body["DETALLE"],$body["SUB_TOTAL"],$body["TOTAL_ISV"],$body["TOTAL"],$body["FECHA_ENTREGA"],$body["ESTADO"]);
+                echo json_encode("Pedido Actualizado");
+            break;
 
-        case "DeletePedido":
-            $datos=$pedidos->delete_pedido($body["ID"]);
-            echo json_encode("Pedido Eliminado");
-        break;
+            case "DeletePedido":
+                $datos=$pedidos->delete_pedido($body["ID"]);
+                echo json_encode("Pedido Eliminado");
+            break;
     }
 ?>
